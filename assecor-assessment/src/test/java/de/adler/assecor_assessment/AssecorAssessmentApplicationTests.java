e52b3811f9348e3dffaaf49ecfe689af216c7197
DenAdler92 <denadler92@gmail.com>
2025-11-07 19:05:30 +0100

    Initialize Spring Boot Project

---
+++ assecor-assessment/src/test/java/de/adler/assecor_assessment/AssecorAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package de.adler.assecor_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssecorAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
