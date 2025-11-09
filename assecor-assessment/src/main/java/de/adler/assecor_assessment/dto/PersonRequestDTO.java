0c19f66cfd6bbe3c4351a8510ea3b67b4837e20c
DenAdler92 <denadler92@gmail.com>
2025-11-09 14:42:55 +0100

    Add PersonRequestDTO

---
+++ assecor-assessment/src/main/java/de/adler/assecor_assessment/dto/PersonRequestDTO.java
@@ -0,0 +1,15 @@
+package de.adler.assecor_assessment.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PersonRequestDTO {
+
+    private String name;
+    private String lastname;
+    private int zipcode;
+    private String city;
+    private int colorCode;
+}
