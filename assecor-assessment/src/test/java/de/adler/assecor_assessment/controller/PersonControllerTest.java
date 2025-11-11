7de80c3aedf61fe729deae90e7332fe1c28e6f37
DenAdler92 <denadler92@gmail.com>
2025-11-11 17:11:59 +0100

    Delete unused import

--- assecor-assessment/src/test/java/de/adler/assecor_assessment/controller/PersonControllerTest.java
+++ assecor-assessment/src/test/java/de/adler/assecor_assessment/controller/PersonControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.http.MediaType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
